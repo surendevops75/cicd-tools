a5915d602c0557be29c98b2d4b2b151210d0a965
surendevops75 <surendevops75@gmail.com>
2025-12-28 12:13:00 +0530

    cicd-tools

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "surendevops.fun"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z10042263JCJ5ZTK2AT9J"
+  description = "description"
+}
